10160bea7651b07e0ede5a08a01b224445e54000
Fascinated Box <FascinatedBox@gmail.com>
2016-10-02 17:25:59 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# spawni
+
+### Version: 0.1
+
+This package provides a wrapper over an interpreter.
+
+You can install this package via
+
+`garden install github FascinatedBox/spawni`
+
+and then use it like so
+
+`import spawni`
+
+## Classes
+
+#### `class Interpreter`
+
+This class provides a wrapper around a Lily interpreter. This class provides a
+constructor that does not take arguments. The parse methods will rewind state on
+failure.
+
+`method Interpreter.error: String`
+
+Returns the error (message and traceback) from the last parse. If there was no
+error, then the result is an empty `String`.
+
+`method Interpreter.parse_expr(context: String, data: String): Option[String]`
+
+Parse an expression that should return a value. 'context' is used as the
+filename in the event of an error. The result is a `Some` containing the
+expression's value into a `String`, or `None`.
+
+`method Interpreter.parse_file(filename: String): Boolean`
+
+Try to open 'filename', parse the contents, and execute them.
+
+`method Interpreter.parse_string(context: String, data: String): Boolean`
+
+Similar to 'parse_expr', except that the contents are not limited to a single
+expression.
---
+++ lily_spawni.c
@@ -0,0 +1,130 @@
+#include "lily_api_alloc.h"
+#include "lily_api_code_iter.h"
+#include "lily_api_embed.h"
+#include "lily_api_msgbuf.h"
+#include "lily_api_value.h"
+
+/**
+package spawni
+
+Provide an interpreter as a value, so you can interpret while you interpret.
+*/
+
+#define CID_INTERPRETER cid_table[0]
+
+/**
+class Interpreter
+
+This class wraps over an interpreter, providing useful methods.
+*/
+typedef struct {
+    LILY_FOREIGN_HEADER_PADDED
+    lily_state *subi;
+} lily_spawni_interpreter;
+
+static void destroy_interpreter(lily_generic_val *g)
+{
+    lily_spawni_interpreter *lsi = (lily_spawni_interpreter *)g;
+    lily_free_state(lsi->subi);
+    lily_free(g);
+}
+
+/**
+constructor Interpreter: Interpreter
+
+Constructs a new `Interpreter`. This operation does not have any side-effects,
+except the underlying allocation required. It is therefore possible to create as
+many `Interpreter` instances as one may need.
+
+The parse methods of this class will all execute code on success. On failure,
+they will rewind state. In theory, variables declared in a failed parse should
+not be available to subsequent passes.
+*/
+void lily_spawni_Interpreter_new(lily_state *s)
+{
+    uint16_t *cid_table = lily_get_cid_table(s);
+
+    lily_spawni_interpreter *lsi = lily_malloc(sizeof(lily_spawni_interpreter));
+    lsi->refcount = 0;
+    lsi->instance_id = CID_INTERPRETER;
+    lsi->subi = lily_new_state(lily_new_default_options());
+    lsi->destroy_func = destroy_interpreter;
+
+    lily_return_foreign(s, (lily_foreign_val *)lsi);
+}
+
+/**
+method Interpreter.error(self: Interpreter): String
+
+Get the full error (message and traceback) from the last parse. If there is no
+error, then the result is a `String`.
+*/
+void lily_spawni_Interpreter_error(lily_state *s)
+{
+    lily_spawni_interpreter *lsi =
+            (lily_spawni_interpreter *)lily_arg_generic(s, 0);
+
+    lily_return_string(s, lily_new_raw_string(lily_get_error(lsi->subi)));
+}
+
+/**
+method Interpreter.parse_expr(self: Interpreter, context: String, data: String): Option[String]
+
+This parses 'data' as an expression that has a result.
+
+On success, a `Some` is returned with the result of the expression turned into a
+`String`. The `String` may be empty if the expression returned an empty result.
+
+On failure, a `None` is returned.
+*/
+void lily_spawni_Interpreter_parse_expr(lily_state *s)
+{
+    lily_spawni_interpreter *lsi =
+            (lily_spawni_interpreter *)lily_arg_generic(s, 0);
+    char *context = lily_arg_string_raw(s, 1);
+    char *text = lily_arg_string_raw(s, 2);
+    lily_value *value;
+    const char *out_text;
+
+    int ok = lily_parse_expr(lsi->subi, context, text, &out_text);
+
+    if (ok) {
+        lily_instance_val *somev = lily_new_some();
+        lily_variant_set_string(somev, 0, lily_new_raw_string(out_text));
+        lily_return_filled_variant(s, somev);
+    }
+    else
+        lily_return_empty_variant(s, lily_get_none(s));
+}
+
+/**
+method Interpreter.parse_file(self: Interpreter, filename: String): Boolean
+
+This attempts to open 'filename' and parse it. If parsing succeeds, then the
+interpreter will attempt to execute the instructions.
+*/
+void lily_spawni_Interpreter_parse_file(lily_state *s)
+{
+    lily_spawni_interpreter *lsi =
+            (lily_spawni_interpreter *)lily_arg_generic(s, 0);
+    char *filename = lily_arg_string_raw(s, 1);
+
+    lily_return_integer(s, lily_parse_file(lsi->subi, filename));
+}
+
+/**
+method Interpreter.parse_string(self: Interpreter, context: String, data: String): Boolean
+
+This parses the content of 'data' as-is. 'context' is used as the source
+filename in the event of an error.
+*/
+void lily_spawni_Interpreter_parse_string(lily_state *s)
+{
+    lily_spawni_interpreter *lsi =
+            (lily_spawni_interpreter *)lily_arg_generic(s, 0);
+    char *context = lily_arg_string_raw(s, 1);
+    char *text = lily_arg_string_raw(s, 2);
+
+    lily_return_integer(s, lily_parse_string(lsi->subi, context, text));
+}
+#include "dyna_spawni.h"
